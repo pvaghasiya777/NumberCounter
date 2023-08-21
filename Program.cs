609bb40868a370cdf5ff19b983f3c1fe384bfb45
pvaghasiya777 <paragvaghasiya777@gmail.com>
2023-08-21 11:56:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace NumberCounter
+{
+
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            MySingleton mysingleton = MySingleton.GetInstance();
+            mysingleton.DoSomething("Console Message From Void Main");
+            mysingleton.Increment();
+            mysingleton.Increment();
+            mysingleton.Increment();
+           // mysingleton.GetCounterNumber();
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public class MySingleton
+    {
+        private static MySingleton instance = new MySingleton();
+        private int count;
+
+        // Private constructor to prevent direct instantiation
+        private MySingleton()
+        {
+            // Initialization code, if any
+        }
+
+        public static MySingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MySingleton();
+            }
+            return instance;
+        }
+
+        private void GetCounterNumber()
+        {
+            count = 0;  
+            Console.WriteLine(count.ToString());
+        }
+        public void Increment()
+        {
+            count++;
+        }
+        public void Decrement()
+        {
+            count--;
+        }
+        public void DoSomething(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+    }
+}
